6f5c279b39b623217f3ce327defc595739b258b4
Furkan Ar <far.farnt@gmail.com>
2023-08-25 19:07:07 +0300

    role not found exception added

---
+++ authentication-service/src/main/java/org/commerce/authenticationservice/exception/role/RoleCannotFoundException.java
@@ -0,0 +1,7 @@
+package org.commerce.authenticationservice.exception.role;
+
+public class RoleCannotFoundException extends RuntimeException {
+    public RoleCannotFoundException(String message) {
+        super(message);
+    }
+}
